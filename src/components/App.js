8508bb855ab7892d4153f86f7a1e501bf410bee7
Joseph <josephshonko99@gmail.com>
2024-05-13 21:14:39 +0300

    commiting components and props

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header"
+import About from "./About"
+import ArticleList from "./ArticleList"
 
 console.log(blogData);
 
 function App() {
+  
+    const posts = [
+      {
+        id: 1,
+        title: "Title 1",
+        date: "April 20, 2022",
+        preview: "Preview of article 1",
+      },
+      {
+        id: 2,
+        title: "Title 2",
+        preview: "Preview of article 2",
+      },
+      {
+        id: 3,
+        title: "Title 3",
+        preview: "Preview of article 3",
+      },
+    ]
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+     <div className="App">
+    //   You're on your own from here! Follow the deliverables; test things out in
+    //   the browser as you write your code; and good luck!
+      <Header name={blogData.name}/>
+     <About image ={blogData.image}
+     about={blogData.about}/>
+     <ArticleList posts={posts} />
+
     </div>
   );
 }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+
+import Article from "./Article"
+
+function ArticleList({posts}) {
+    const articles = posts.map((post) => (
+        <Article 
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+        />
+    ))
+  return (
+    <main>
+     {articles}  
+    </main>
+  )
+}
+
+export default ArticleList;
